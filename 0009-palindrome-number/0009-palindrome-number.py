7bf70a60cb592693030b8b7417ea4faecddec23e
Valerie Tsai <118166488+valerietsai@users.noreply.github.com>
2025-08-28 10:56:35 +0800

    Time: 2 ms (90.83%), Space: 17.8 MB (64.4%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # 負數不可能是迴文
-        if x < 0:
+        # 負數，或者個位數是 0 但本身不是 0 的數字，不可能是迴文
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        
-        # 將整數轉為字串，然後比較字串與其反轉後是否相等
-        return str(x) == str(x)[::-1]
+
+        reverted_number = 0
+
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x //= 10
+            
+        return x == reverted_number or x == reverted_number // 10
